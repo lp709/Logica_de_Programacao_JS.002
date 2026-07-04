f0e2dfb60b8dec9ad5699610d3783633e14b7a30
Lp <pineiroleandro7009@gmail.com>
2026-07-04 08:36:03 +0100

    Logica de programacao, diretorio 002

---
+++ LogicaDeProgramacao/aula1.js
@@ -0,0 +1,6 @@
+var numero = 5.7;
+var nome = 'jesus';
+var dirige = !false;
+console.log(numero)
+console.log(nome)
+console.log(dirige)
---
+++ LogicaDeProgramacao/aula5.js
@@ -0,0 +1,7 @@
+//o nome temque ser escrito quando estiver executando o programa
+//node aula5.js Leandro
+//output: Leandro
+
+const nome = process.argv[2];
+console.log(nome);
+
